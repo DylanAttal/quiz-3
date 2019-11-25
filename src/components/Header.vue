3a9d7ed4a761fc2e86a356a2237870f9d99f9c75
Dylan Attal <attal.dylan@gmail.com>
2019-11-25 17:04:29 -0500

    Deletes boilerplate. Makes Header.

---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>My Vue Scoreboard</h1>
+  </div>
+</template>
+
+<style scoped>
+div {
+  background-color: #eee;
+}
+
+h1 {
+  margin: 0;
+  padding: 1rem;
+}
+</style>
