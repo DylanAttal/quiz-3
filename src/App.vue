61f982c42bab90486b89f8a565c157affbb480ce
Dylan Attal <attal.dylan@gmail.com>
2019-11-25 17:14:03 -0500

    passes down team name via props

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,7 @@
   <div id="app">
     <Header />
     <div class="team-container">
-      <Team />
-      <Team />
+      <Team v-for="(team, index) in this.teams" :key="index" :teamName="team.teamName" />
     </div>
   </div>
 </template>
@@ -17,6 +16,18 @@ export default {
   components: {
     Header,
     Team
+  },
+  data() {
+    return {
+      teams: [
+        {
+          teamName: "Gators"
+        },
+        {
+          teamName: "Seminoles"
+        }
+      ]
+    };
   }
 };
 </script>
